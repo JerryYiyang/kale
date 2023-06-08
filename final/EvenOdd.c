d02352b5174edb1acb480d97b4ec47cb6e0f87c9
Jerry Huo <66449672+JerryYiyang@users.noreply.github.com>
2023-06-07 22:28:58 -0700

    still working

---
+++ final/EvenOdd.c
@@ -0,0 +1,96 @@
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+
+void oddEven(int arr[], int n, int id, int p);
+void swap(int arr[], int i, int j, int *sorted);
+void synch(int par_id,int par_count,int *ready);
+
+int *ready;
+
+int main(int argc, char *argv[]){
+    char num[10000], *token;
+    int p, i, f, *nums, n, count;
+    read(STDIN_FILENO, num, 10000);
+    p = atoi(argv[1]);
+    n = strlen(num) / 3;
+    nums = mmap(NULL, sizeof(int) * n, PROT_READ | PROT_WRITE, MAP_SHARED | 0x20, -1, 0);
+    token = strtok(num, " ");
+    printf("initial array: [ ");
+    count = 0;
+    while(token != NULL){
+        nums[count] = atoi(token);
+        printf("%d, ", nums[count]);
+        token = strtok(NULL, " ");
+        count++;
+    }
+    printf("]\n");
+    if((count / p) < 2){
+        p = count / 2;
+    }
+    ready = mmap(NULL, sizeof(int) * p, PROT_READ | PROT_WRITE, MAP_SHARED | 0x20, -1, 0);
+    for(i = 0; i < p; i++){
+        f = fork();
+        if(f == 0){
+            oddEven(nums, count, i, p);
+            exit(0);
+        }
+    }
+    for(i = 0; i < p; i++){
+        wait(0);
+    }
+    printf("sorted array:  [ ");
+    for(i = 0; i < count; i++){
+        printf("%d, ", nums[i]);
+    }
+    printf("]\n");
+    printf("processes: %d\n", p);
+    munmap(ready, sizeof(int) * p);
+    munmap(nums, sizeof(int) * n);
+    return 0;
+}
+
+void oddEven(int arr[], int n, int id, int p){
+    int sorted = 0;
+    while (!sorted) {
+        sorted = 1;
+        for (int i = 1; i <= n - 2; i += 2){
+            if(i % p == id){
+                swap(arr, i, i + 1, &sorted);
+            }
+        }
+        synch(id, p, ready);
+        for (int i = 0; i <= n - 2; i += 2){
+            if(i % p == id){
+                swap(arr, i, i + 1, &sorted);
+            }
+        }
+        synch(id, p, ready);
+    }
+}
+
+void swap(int arr[], int i, int j, int *sorted){
+    if (arr[i] > arr[j]) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        *sorted = 0;
+    }
+}
+
+void synch(int par_id,int par_count,int *ready)
+{
+    int i;
+    ready[par_id] += 1;
+    for(i = 0; i < par_count; i++){
+        while(ready[par_id] > ready[i]);
+    }
+}
---
+++ final/Randomizer.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+
+int main(int argc, char *argv[]){
+    int n, i, j, temp, f, fd[2];
+    char *p, *nums;
+    n = atoi(argv[1]);
+    p = argv[2];
+    pipe(fd);
+    f = fork();
+    if(f == 0){
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO); 
+        execlp("./EvenOdd", "./EvenOdd", argv[2], NULL);
+    } else{
+        close(fd[0]);
+        nums = malloc(sizeof(char) * 5 * n);
+        srand(time(NULL));
+        j = 0;
+        for (i = 0; i < n; i++) {
+            temp = (rand() % 201) - 100;
+            j += sprintf(nums + j, "%d ", temp);
+        }
+        nums[j - 1] = '\n';
+        write(fd[1], nums, strlen(nums));
+        close(fd[1]);
+        free(nums);
+        wait(0);
+    }
+    return 0;
+}
