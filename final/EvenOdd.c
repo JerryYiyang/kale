0f3593045c0ecfe69d37247068f5f321e04b23b8
Jerry Huo <66449672+JerryYiyang@users.noreply.github.com>
2023-06-08 11:21:09 -0700

    still working

--- final/EvenOdd.c
+++ final/EvenOdd.c
@@ -10,6 +10,8 @@
 #include <sys/types.h>
 #include <time.h>
 
+
+/*doesnt work if im calling from a.out for some reason*/
 void oddEven(int arr[], int n, int id, int p);
 void swap(int arr[], int i, int j, int *sorted);
 void synch(int par_id,int par_count,int *ready);
@@ -21,22 +23,29 @@ int main(int argc, char *argv[]){
     int p, i, f, *nums, n, count;
     read(STDIN_FILENO, num, 10000);
     p = atoi(argv[1]);
-    n = strlen(num) / 3;
+    n = strlen(num) / 2;
     nums = mmap(NULL, sizeof(int) * n, PROT_READ | PROT_WRITE, MAP_SHARED | 0x20, -1, 0);
     token = strtok(num, " ");
-    printf("initial array: [ ");
     count = 0;
     while(token != NULL){
         nums[count] = atoi(token);
-        printf("%d, ", nums[count]);
         token = strtok(NULL, " ");
         count++;
     }
-    printf("]\n");
+    printf("initial array: [ ");
+    for(i = 0; i < count; i++){
+        if(i != (count - 1)){
+            printf("%d, ", nums[i]);
+        }
+    }
+    printf("%d ]\n", nums[count - 1]);
     if((count / p) < 2){
         p = count / 2;
     }
     ready = mmap(NULL, sizeof(int) * p, PROT_READ | PROT_WRITE, MAP_SHARED | 0x20, -1, 0);
+    for(i = 0; i < p; i++){
+        ready[i] = 0;
+    }
     for(i = 0; i < p; i++){
         f = fork();
         if(f == 0){
@@ -49,9 +58,11 @@ int main(int argc, char *argv[]){
     }
     printf("sorted array:  [ ");
     for(i = 0; i < count; i++){
-        printf("%d, ", nums[i]);
+        if(i != (count - 1)){
+            printf("%d, ", nums[i]);
+        }
     }
-    printf("]\n");
+    printf("%d ]\n", nums[count - 1]);
     printf("processes: %d\n", p);
     munmap(ready, sizeof(int) * p);
     munmap(nums, sizeof(int) * n);
@@ -62,6 +73,7 @@ void oddEven(int arr[], int n, int id, int p){
     int sorted = 0;
     while (!sorted) {
         sorted = 1;
+        synch(id, p, ready);
         for (int i = 1; i <= n - 2; i += 2){
             if(i % p == id){
                 swap(arr, i, i + 1, &sorted);
@@ -73,7 +85,6 @@ void oddEven(int arr[], int n, int id, int p){
                 swap(arr, i, i + 1, &sorted);
             }
         }
-        synch(id, p, ready);
     }
 }
 
