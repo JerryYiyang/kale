3fbddd954faecb5cb398025a7721d25b7e1bc80d
Jerry Huo <66449672+JerryYiyang@users.noreply.github.com>
2023-06-09 17:25:23 -0700

    done

--- final/EvenOdd.c
+++ final/EvenOdd.c
@@ -9,18 +9,22 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
+#include <sys/time.h>
 
 /*doesnt work if im calling from a.out for some reason*/
-void oddEven(int arr[], int n, int id, int p);
+void oddEven(int arr[], int n, int id, int p, int size);
 void swap(int arr[], int i, int j, int *sorted);
 void synch(int par_id,int par_count,int *ready);
 
 int *ready;
 
 int main(int argc, char *argv[]){
+    struct timeval start, end;
+    long mtime, seconds, useconds;
     char num[10000], *token;
     int p, i, f, *nums, n, count;
     read(STDIN_FILENO, num, 10000);
+    gettimeofday(&start, NULL);
     p = atoi(argv[1]);
     n = strlen(num) / 2;
     nums = mmap(NULL, sizeof(int) * n, PROT_READ | PROT_WRITE, MAP_SHARED | 0x20, -1, 0);
@@ -43,21 +47,22 @@ int main(int argc, char *argv[]){
     if((count / p) < 2){
         p = count / 2;
     }
-    ready = mmap(NULL, sizeof(int) * (p + 1), PROT_READ | PROT_WRITE, MAP_SHARED | 0x20, -1, 0);
-    for(i = 0; i < (p + 1); i++){
+    ready = mmap(NULL, sizeof(int) * p, PROT_READ | PROT_WRITE, MAP_SHARED | 0x20, -1, 0);
+    for(i = 0; i < p; i++){
         ready[i] = 0;
     }
     for(i = 0; i < p; i++){
         f = fork();
         if(f == 0){
             synch(i, p, ready);
-            oddEven(nums, count, i, p);
+            oddEven(nums, count, i, p, count);
             exit(0);
         }
     }
     for(i = 0; i < p; i++){
         wait(0);
     }
+    gettimeofday(&end, NULL);
     printf("sorted array:  [ ");
     for(i = 0; i < count; i++){
         if(i != (count - 1)){
@@ -65,24 +70,33 @@ int main(int argc, char *argv[]){
         }
     }
     printf("%d ]\n", nums[count - 1]);
-    printf("processes: %d\n", p);
+    printf("\nprocesses: %d\n", p);
+    seconds  = end.tv_sec  - start.tv_sec;
+    useconds = end.tv_usec - start.tv_usec;
+    mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
+    printf("time to sort: %ld ms\n", mtime);
     munmap(ready, sizeof(int) * p);
     munmap(nums, sizeof(int) * n);
     return 0;
 }
 
-void oddEven(int arr[], int n, int id, int p){
+void oddEven(int arr[], int n, int id, int p, int size){
+    int j;
     int sorted = 0;
-    while (!sorted){
+    for(j = 0; j <= (size / 2); j++){
         sorted = 1;
+        synch(id, p, ready);
         for (int i = 1; i <= n - 2; i += 2){
-            swap(arr, i, i + 1, &sorted);
+            if(i % p == id){
+                swap(arr, i, i + 1, &sorted);
+            }
         }
         synch(id, p, ready);
         for (int i = 0; i <= n - 2; i += 2){
-            swap(arr, i, i + 1, &sorted);
+            if(i % p == id){
+                swap(arr, i, i + 1, &sorted);
+            }
         }
-        synch(id, p, ready);
     }
 }
 
@@ -98,16 +112,8 @@ void swap(int arr[], int i, int j, int *sorted){
 void synch(int par_id,int par_count,int *ready)
 {
     int i;
-    // ready[par_id] += 1;
-    // for(i = 0; i < par_count; i++){
-    //     while(ready[par_id] > ready[i]);
-    // }
-
-    if(par_id == 0){
-        ready[par_count] += 1;
-    }
     ready[par_id] += 1;
     for(i = 0; i < par_count; i++){
-        while(ready[par_count] > ready[i]);
+        while(ready[par_id] > ready[i]);
     }
 }
